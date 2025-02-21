250aa67a070e0287a1067bd7447d7d0eb4df4c52
uhinnce <112843891+Cmdmsinfo32@users.noreply.github.com>
2025-02-21 18:01:03 +0800

    Delete chapter3 directory

--- chapter3/test2.c
+++
@@ -1,8 +0,0 @@
-// test 2 -- 第二题
-#include<stdio.h>
-int main(){
-    printf("Startled by the sudden sound, Sally shouted,\n\"By the Great Pumpkin, what was that!\"\n");
-
-    getchar();
-    return 0;
-}
